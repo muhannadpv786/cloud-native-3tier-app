b5f195803768b9158798024b976b288a5cf8cc56
MUHANNED PV <p.vmuhannad786@gmail.com>
2026-08-01 20:40:52 +0530

    Update EC2 and VPC outputs to new instance names

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,14 @@
 output "ec2_public_ip" {
   description = "Public IP of EC2 instance"
-  value       = aws_instance.efootball25_ec2.public_ip
+  value       = aws_instance.muhannad_ec2.public_ip
 }
 
 output "ec2_public_dns" {
   description = "Public DNS of EC2 instance"
-  value       = aws_instance.efootball25_ec2.public_dns
+  value       = aws_instance.muhannad_ec2.public_dns
 }
 
 output "vpc_id" {
   description = "VPC ID"
-  value       = aws_vpc.efootball25_vpc.id
+  value       = aws_vpc.muhannad_vpc.id
 }
